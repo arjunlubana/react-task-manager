07ca5607c2c02b0d3cd42473f3e9bf17c067571d
Grayson Carter <graysoncarter1999@gmail.com>
2021-11-20 08:33:22 +0300

    Removed completedTasks state, filtered tasks state to show completed tasks

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,12 @@
 export default function Layout({ children }) {
-
-    const showCompleted = () => {
-
-    }
-
-    const showPending = () => {
-
-    }
-    
     return (
         <div>
             <header className="header">
                 <h1>Task Manager</h1>
                 <nav className="nav-container">
                     <ul className="nav-list">
-                        <li className="nav-item"><button onClick={showPending}>Pending</button></li>
-                        <li className="nav-item"><button onClick={showCompleted}>Completed</button></li>
+                        <li className="nav-item"><button>Pending</button></li>
+                        <li className="nav-item"><button>Completed</button></li>
                     </ul>
                 </nav>
             </header>
--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,85 +1,64 @@
-import AddTask from './AddTask'
-import PendingTasks from './pendingTasks'
-import CompletedTasks from './completedTasks'
-import { useState } from 'react'
+import AddTask from "./AddTask";
+import PendingTasks from "./pendingTasks";
+import CompletedTasks from "./completedTasks";
+import { useState } from "react";
 
 export default function TasksManager() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Wash Dishes",
+      description: "Make them clean",
+      completed: false,
+      date: new Date(2010, 11, 10).toDateString(),
+    },
+    {
+      id: 2,
+      title: "Wash Clothes",
+      description: "Make them clean",
+      completed: false,
+      date: new Date(2010, 11, 10).toDateString(),
+    },
+    {
+      id: 3,
+      title: "Wash Face",
+      description: "Make them clean",
+      completed: false,
+      date: new Date(2010, 11, 10).toDateString(),
+    },
+  ]);
 
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: "Wash Dishes",
-            description: "Make them clean",
-            completed: false,
-            date: new Date(2010, 11, 10).toDateString()
-        },
-        {
-            id: 2,
-            title: "Wash Clothes",
-            description: "Make them clean",
-            completed: false,
-            date: new Date(2010, 11, 10).toDateString()
-        },
-        {
-            id: 3,
-            title: "Wash Face",
-            description: "Make them clean",
-            completed: false,
-            date: new Date(2010, 11, 10).toDateString()
-        }
-    ])
+  const [completedTasks, setCompletedTasks] = useState([]);
 
-    const [completedTasks, setCompletedTasks] = useState([
-    ])
+  const addTask = (task) => {
+    setTasks([task, ...tasks]);
+  };
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-    const addTask = (task) => {
+  const completeTask = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
 
-        setTasks(
-            [task, ...tasks]
-        )
-    }
-
-    const deleteTask = (id) => {
-        setTasks(tasks.filter((task) => task.id !== id))
-    }
-
-    const completeTask = (id) => {
-        setTasks(tasks.map((task) => {
-            if (task.id === id) {
-                task = { ...task, completed: !task.completed }
-                setCompletedTasks([...completedTasks].concat(task))
-                return task
-            } else {
-                return task
-            }
-        }))
-        setTimeout(() => setTasks(tasks.filter((task) => task.id !== id)), 250)
-    }
-
-    const deleteCompleted = (id) => {
-        setCompletedTasks(completedTasks.filter((task) => task.id !== id))
-    }
-
-    const inComplete = (id) => {
-        setCompletedTasks(completedTasks.map((task) => {
-            if (task.id === id) {
-                task = { ...task, completed: !task.completed }
-                setTasks([...tasks].concat(task))
-                return task
-            } else {
-                return task
-            }
-        }))
-        setTimeout(() => setCompletedTasks(completedTasks.filter((task) => task.id !== id)), 250)
-    }
-
-    return (
-        <div>
-            <AddTask onSubmit={addTask} />
-            <PendingTasks tasks={tasks} onDelete={deleteTask} onComplete={completeTask}/>
-            <CompletedTasks tasks={completedTasks} onDelete={deleteCompleted} onComplete={inComplete} />
-
-        </div>
-    )
+  return (
+    <div>
+      <AddTask onSubmit={addTask} />
+      <PendingTasks
+        tasks={tasks.filter((task) => task.completed === false)}
+        onDelete={deleteTask}
+        onComplete={completeTask}
+      />
+      <CompletedTasks
+        tasks={tasks.filter((task) => task.completed === true)}
+        onDelete={deleteTask}
+        onComplete={completeTask}
+      />
+    </div>
+  );
 }
